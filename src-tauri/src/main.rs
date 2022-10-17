2dc0fdc7cb08f424234732028b622640620cdf37
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-10-17 16:02:44 -0300

    add styled components and create git add command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,19 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::process::Command;
+
+#[tauri::command]
+fn git_add(file_path: &str) {
+  let mut git = Command::new("git");
+  git.args(["add", file_path]).spawn().expect("");
+}
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![git_add])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
